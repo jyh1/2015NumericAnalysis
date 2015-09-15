68d0e08e3602925cad5437f16b51b8a46abd7fa8
jyh1 <jyh1@mail.ustc.edu.cn>
2015-09-15 20:34:11 +0800

    homework week 2

--- homework1/homework.c
+++ homework1/homework.c
@@ -1,9 +1,27 @@
-#include "stdio.h"
+#include <stdio.h>
+#include "math.h"
+#define Infinity 2147483657
+#define Tolearate 0.0000001
 
 float Sum(float x) {
   int k;
-  float sum;
-  for (i = 0)
+  float sum=0, temp;
+  for (k = 1; k < Infinity; k++) {
+    temp = 1/(k * (k + x));
+    if (temp < Tolearate) break;
+    sum += temp;
+    /* code */
+  }
+  printf("x = %.1f,y = %.15f\n", x, sum );
   /* code */
+  return 0;
+}
+int main(){
+  int n = 7;
+  float x[7] = {0, 0.5, 1, sqrt(2), 10, 100, 300};
+  int i;
+  for (i = 0; i <= n - 1; i++){
+    Sum (x[i]);
+  }
   return 0;
 }
